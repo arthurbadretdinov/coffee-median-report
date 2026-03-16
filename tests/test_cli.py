0dc855aec619f6ea16b3602b5b1aefdce97b0a04
Artur Badretdinov <artur.badretdinov11@gmail.com>
2026-03-16 15:21:26 +0500

    Split parse_arguments tests into error and success cases

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -25,17 +25,57 @@ from cli import parse_arguments
         ), # чтение трёх файлов
         (
             [
+                "--report", 
+                "median-coffee",
                 "--files", 
                 "data/math.csv", 
                 "data/physics.csv", 
-                "data/programming.csv", 
-                "--report", 
-                "median-coffee"
+                "data/programming.csv"
             ], does_not_raise()
         ), # чтение трёх файлов
     ]
 )
-
-def test_parse_arguments(args_list, expectation):
+def test_parse_arguments_error(args_list, expectation):
     with expectation:
         parse_arguments(args_list)
+        
+        
+@pytest.mark.parametrize(
+    "args_list, files, report", 
+    [
+        (
+            ["--files", "data/math.csv", "--report", "median-coffee"],
+            ["data/math.csv"],
+            "median-coffee"
+        ), 
+        (
+            [
+                "--files", 
+                "data/math.csv", 
+                "data/physics.csv", 
+                "data/chemistry.csv", 
+                "--report", 
+                "median-coffee"
+            ],
+            ["data/math.csv", "data/physics.csv", "data/chemistry.csv"],
+            "median-coffee"
+        ),
+        (
+            [
+                "--report", 
+                "median-coffee",
+                "--files", 
+                "data/math.csv", 
+                "data/physics.csv", 
+                "data/programming.csv"
+            ], 
+            ["data/math.csv", "data/physics.csv", "data/programming.csv"],
+            "median-coffee"
+        ),
+    ]
+)
+def test_parse_arguments_correct(args_list, files, report):
+    args = parse_arguments(args_list)
+    
+    assert args.files == files
+    assert args.report == report
